3c07dbf6b91184cd5dd4eec625796a18b7fa9092
lakshmiyvs <yvs.lakshmi@gmail.com>
2024-12-18 10:47:58 -0600

    start of the case studyy project

---
+++ src/main/java/com/casestudy/webapp/PurrfectPawsGroomingApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurrfectPawsGroomingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurrfectPawsGroomingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casestudy/webapp/PurrfectPawsGroomingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casestudy.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurrfectPawsGroomingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
